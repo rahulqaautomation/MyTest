534845b789128d799f67c25bf7be61d89e9b8384
sandeep kumar <rinkiy311@gmail.com>
2021-09-28 19:55:40 +0530

    My first commit

---
+++ program6.java
@@ -0,0 +1,13 @@
+class program6
+{
+public static void main(String[] args)
+{
+     for(int i=1; i<4; i++);
+{
+     for(j=1; j<i; j++)
+{
+     System.out.println("*");
+}
+     System.out.println();
+  }
+}
